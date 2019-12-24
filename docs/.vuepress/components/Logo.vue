586f0ba99b5eefaf96439358daaba2a805b4bb3a
Oz Ramos <heyozramos@gmail.com>
2019-12-23 16:39:49 -0800

    #4 Adds logo animation

--- docs/.vuepress/components/Logo.vue
+++ docs/.vuepress/components/Logo.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <LogoSVG class="logo-animated-slowly" :style="styles" />
+  <div style="height: 151px; position: relative;">
+    <LogoSVG
+      class="logo-animated-slowly handsfree-show-when-stopped-or-loading"
+      :style="styles"
+    />
+    <div
+      ref="animatedLogoWrap"
+      class="handsfree-show-when-started"
+      style="position: relative; left: -100px"
+    >
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+      <LogoSVG class="logo-animated" :style="styles" />
+    </div>
   </div>
 </template>
 
@@ -9,6 +30,22 @@ import LogoSVG from './LogoSVG'
 
 export default {
   components: { LogoSVG },
-  props: ['styles']
+  props: ['styles'],
+
+  mounted() {
+    document.addEventListener('started', () => {
+      const bounds = this.$refs.animatedLogoWrap.getBoundingClientRect()
+
+      window.confetti({
+        startVelocity: 35,
+        spread: 360,
+        particleCount: 1000,
+        origin: {
+          x: (bounds.left + 100) / window.innerWidth,
+          y: (bounds.top + 75) / window.innerHeight
+        }
+      })
+    })
+  }
 }
 </script>
